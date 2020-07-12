14f4e9ef388e3961c01d59eb0988dbcfe245f14c
Luciano de Souza <luci0871@stud.kea.dk>
2020-07-12 21:33:02 +0200

    changes

--- OneDrive/Área de Trabalho/try-timeline/jscript.js
+++ OneDrive/Área de Trabalho/try-timeline/jscript.js
@@ -1,35 +1,67 @@
-window.addEventListener("DOMContentLoaded", init);
+/* window.addEventListener("DOMContentLoaded", init);
 
-function init() {
-    step();
 
-}
-$(".step").click(function () {
-    $(this).addClass("active").prevAll().addClass("active");
-    $(this).nextAll().removeClass("active");
+var select = function (s) {
+        return document.querySelector(s);
+    },
+    selectAll = function (s) {
+        return document.querySelectorAll(s);
+    },
+    svg = $("svg"),
+    bulb_full = select("#bulb"),
+    bulb = select("#bulb_body"),
+    licht = select("#licht"),
+    glow_on = select("#glow");
+
+$('#bulb-svg').on("load", function () {
+    var licht_init = new TimelineLite();
+    TweenMax.set(licht, {
+        opacity: 0,
+        scale: 0.6,
+        transformOrigin: "50% 50%"
+    });
+    TweenMax.set(glow, {
+        opacity: 1,
+        transformOrigin: "50% 50%",
+        stroke: "#3D3D3D"
+    });
 });
 
-$(".step01").click(function () {
-    $("#line-progress").css("width", "3%");
-    $(".discovery").addClass("active").siblings().removeClass("active");
+$('#bulb-svg').on('mouseenter', function (i) {
+    TweenMax.to(bulb_body, 0.3, {
+        opacity: 1,
+        fill: "#96BF1F"
+    });
+    TweenMax.to(licht, 0.3, {
+            opacity: 1,
+            scale: 1
+        }),
+        TweenMax.to(glow, 0.3, {
+            opacity: 1,
+            stroke: "#FFFFFF"
+        });
+    TweenMax.fromTo(licht, 1, {
+        scale: 1,
+
+    }, {
+        scale: 1.1,
+        repeat: -1,
+        yoyo: true
+    });
+
 });
 
-$(".step02").click(function () {
-    $("#line-progress").css("width", "25%");
-    $(".strategy").addClass("active").siblings().removeClass("active");
-});
-
-$(".step03").click(function () {
-    $("#line-progress").css("width", "50%");
-    $(".creative").addClass("active").siblings().removeClass("active");
-});
-
-$(".step04").click(function () {
-    $("#line-progress").css("width", "75%");
-    $(".production").addClass("active").siblings().removeClass("active");
-});
-
-$(".step05").click(function () {
-    $("#line-progress").css("width", "100%");
-    $(".analysis").addClass("active").siblings().removeClass("active");
-});
+$('#bulb-svg').on('mouseleave', function (i) {
+    TweenMax.to(bulb_body, 0.3, {
+        opacity: 1,
+        fill: "#FFFFFF"
+    });
+    TweenMax.to(licht, 0.3, {
+            opacity: 0,
+            scale: 0.8
+        }),
+        TweenMax.to(glow_on, 0.3, {
+            opacity: 1,
+            stroke: "#3D3D3D"
+        });
+}); */
