e715a262dcede903b2c10c3b222a9422aabe11be
Luciano de Souza <luci0871@stud.kea.dk>
2021-01-23 21:47:16 +0100

    cleaned up html

---
+++ OneDrive/Área de Trabalho/foobar_order/store.js
@@ -0,0 +1,60 @@
+const endPoint = "https://foobar32.herokuapp.com/";
+
+window.addEventListener("load", init);
+
+function init() {
+  fetch(endPoint)
+    .then((res) => res.json())
+    .then(buildDOM);
+}
+
+setInterval(() => {
+  //document.querySelector("#taps").innerHTML = "";
+  fetch(endPoint)
+    .then((res) => res.json())
+    .then(updateDOM);
+}, 4000);
+
+function buildDOM(data) {
+  //console.log(data);
+  buildTaps(data.storage);
+}
+
+function updateDOM(data) {
+  updateTaps(data.storage);
+}
+function updateTaps(beers) {
+  beers.forEach((beer) => {
+    const element = document.querySelector(`.beer[data-beername='${beer.name}']`);
+   /*  element.querySelector(
+      "p"
+    ).textContent = `${tap.level} out of ${tap.capacity}`;
+    //console.log(element);
+    element.querySelector(".inner").style.width =
+      (tap.level / tap.capacity) * 100 + "%"; */
+  });
+}
+function buildTaps(beers) {
+  //console.log(taps);
+  //grab the template
+  const template = document.querySelector("#storageTemp").content;
+  //loop through the taps
+  beers.forEach((beer) => {
+    //console.log(tap);
+    //3 clone the taps
+    const myBeer = template.cloneNode(true);
+    myBeer.querySelector("article").dataset.beername = beer.name;
+    //4 popultate
+    myBeer.querySelector("h2").textContent = beer.name;
+    myBeer.querySelector("img").setAttribute("src", "Images" + beer.image + "-sm.png")
+    /* myTap.querySelector(
+      "p"
+    ).textContent = `${tap.level} out of ${tap.capacity}`;
+    //5 append to DOM
+    myTap.querySelector(".inner").style.width =
+      (tap.level / tap.capacity) * 100 + "%"; */
+    document.querySelector("#beers").appendChild(myBeer);
+  });
+}
+
+copy.querySelector("img").setAttribute("src", "imgs/small/" + dish.image + "-sm.jpg");
